6d7c254db3a82f7102bf310bd763202e3cd5b099
LovelyLove <106236316+ukm4ks@users.noreply.github.com>
2024-04-05 21:40:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const numStars = 300;
-    const splashScreen = document.getElementById('splash-screen');
-
-    for (let i = 0; i < numStars; i++) {
-        const star = document.createElement('div');
-        star.className = 'star';
-        splashScreen.appendChild(star);
-        const xy = getRandomPosition();
-        star.style.top = xy[0] + 'px';
-        star.style.left = xy[1] + 'px';
-        const speed = Math.random() * 2 + 1;
-        star.style.animation = `twinkle ${speed}s linear infinite`;
-    }
-
-    // Убрать заставку через некоторое время (например, 4 секунды)
-    setTimeout(function() {
-        splashScreen.classList.add('loaded');
-    }, 4000);
-});
+const numStars = 300;
+for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+    document.getElementById('splash-screen').appendChild(star);
+    const xy = getRandomPosition();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    const speed = Math.random() * 2 + 1;
+    star.style.animation = `twinkle ${speed}s linear infinite`;
+}
 
 function getRandomPosition() {
     const x = window.innerWidth * Math.random();
     const y = window.innerHeight * Math.random();
     return [y, x];
 }
-
 
 document.addEventListener('DOMContentLoaded', function() {
     var splashScreen = document.getElementById('splash-screen');
