81b2a2ed376d11024cc7cbd2923f85032daae866
LovelyLove <106236316+ukm4ks@users.noreply.github.com>
2024-05-16 21:07:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,7 @@
-const numStars = 300;
-for (let i = 0; i < numStars; i++) {
-    const star = document.createElement('div');
-    star.className = 'star';
-    document.getElementById('splash-screen').appendChild(star);
-    const xy = getRandomPosition();
-    star.style.top = xy[0] + 'px';
-    star.style.left = xy[1] + 'px';
-    const speed = Math.random() * 2 + 1;
-    star.style.animation = `twinkle ${speed}s linear infinite`;
+function openSupport() {
+    document.getElementById("supportChat").style.display = "block";
 }
 
-function getRandomPosition() {
-    const x = window.innerWidth * Math.random();
-    const y = window.innerHeight * Math.random();
-    return [y, x];
+function closeSupport() {
+    document.getElementById("supportChat").style.display = "none";
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var splashScreen = document.getElementById('splash-screen');
-    setTimeout(function() {
-        splashScreen.classList.add('loaded');
-        showSection('home');
-    }, 4000);
-});
-
-function showSection(sectionId) {
-    var sections = document.querySelectorAll('section');
-    sections.forEach(function(section) {
-        if (section.id === sectionId) {
-            section.style.display = 'block';
-        } else {
-            section.style.display = 'none';
-        }
-    });
-}
-
